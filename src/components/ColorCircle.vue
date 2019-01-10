36bdd69a8a61e41ba3edae1cadb7aaa6561dd68d
luke-hanwook <gksdnr89@gmail.com>
2019-01-10 13:34:29 +0900

    스타일 수정 및 빌드

---
+++ src/components/ColorCircle.vue
@@ -0,0 +1,11 @@
+<template>
+  <transition :name="circleName">
+    <span :id="circleId"></span>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: ['circleId', 'circleName']
+}
+</script>
---
+++ src/components/ColorMenu.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="color"
+    @mouseover="show = true"
+    @mouseleave="show = false"
+    @click="onClickSetColor">
+
+    <colorcircle
+      v-if="show"
+      v-for="(item, index) in colorList"
+      :circle-id="item"
+      :circle-name="`fade${colorList.length - index}`"
+      :key="item">
+    </colorcircle>
+
+    <span class="fixed-color-circle"></span>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+import colorcircle from './ColorCircle.vue'
+
+export default {
+  data() {
+    return {
+      show: false,
+      colorList: ['primary', 'mars', 'saturn', 'neptune']
+    }
+  },
+  components: {colorcircle},
+  methods: {
+    ...mapMutations([
+      'CHANGE_MAIN_COLOR'
+    ]),
+    onClickSetColor({target}) {
+      if(target.id) this.CHANGE_MAIN_COLOR(target.id)
+    }
+  }
+}
+</script>
