09e78de3a9c3412eb64f6ac95f9add9d53018d2e
luke-hanwook <gksdnr89@gmail.com>
2019-01-11 18:57:59 +0900

    modified data

--- src/models/Skills.js
+++ src/models/Skills.js
@@ -1,48 +1,44 @@
 export default {
   data: [
     {
-      title: 'Main Language',
+      title: 'Technical skills',
       description: [
         {
           name: 'Java',
           level: 'middle',
           description: [
-            'Spring Framework를 이용한 프로젝트에 실무경험이 있습니다.(설계, 개발, 배포)',
-            'Java관련 Document들을 분석하는데 어렵지 않고, 참조하여 개발을 진행할 수 있습니다.',
-            'Java언어를 통해 객체지향적인 프로그래밍을 적용할 수 있습니다.(상속, 캡슐화, 다형성)',
-            '상황에 맞게 적절한 자료구조(컬렉션 프레임워크)를 사용합니다.'
+            'Spring Framework를 이용한 프로젝트에 실무경험이 있습니다. (설계, 개발, 배포)',
+            'Java관련 Document들을 읽고 분석하는데 어려움이 없으며, 참조하여 개발을 진행할 수 있습니다.',
+            'Java언어를 통해 객체지향적인 프로그래밍을 적용할 수 있습니다. (상속, 캡슐화, 다형성)',
+            '상황에 맞는 적절한 자료구조(컬렉션 프레임워크)를 사용합니다.'
           ]
         },
         {
           name: 'Java Script',
           level: 'middle',
           description: [
-            '자바스크립트만의 특징을 이해하며 개발에 적용할 수 있습니다.(함수, 프로토타입, 클로저)',
             '각종 JS 라이브러리 사용 시 Document를 읽고 분석하여 커스터마이징이 가능합니다.',
-            'JQuery에 아주 익숙하지만 Vanilla JS로의 구현도 가능합니다.',
-            '모던 프론트엔드 개발을 지향합니다.(ES6와 vue.js의 실무 개발경험이 있습니다.)'
+            '자바스크립트만의 특징을 이해하며 개발에 적용할 수 있습니다.',
+            'JQuery에 아주 익숙하며 Vanilla JS로의 구현도 가능합니다.',
+            '모던 프론트엔드 개발을 지향합니다. (ES6와 vue.js의 실무 개발경험)'
           ]
-        }
-      ]
-    },
-    {
-      title: 'Others',
-      description: [
+        },
         {
           name: 'HTML/CSS',
           level: 'middle',
           description: [
             'HTML과 CSS의 표준 가이드라인을 준수합니다.',
             '웹 페이지의 Search Engine Optimization의 실무 경험이 있습니다.',
-            '반응형 웹과 크로스 브라우징을 적용한 실무 경험이 있습니다.'
+            '반응형 웹과 크로스 브라우징을 적용할 수 있습니다.',
+            'CSS Preprocessor(Sass)의 문법을 알고 사용할 수 있습니다.'
           ]
         },
         {
           name: 'DB',
           level: 'middle',
           description: [
-            'Oracle 및 MySql의 쿼리문법에 익숙합니다.',
             'Spring과 myBatis를 이용한 DB연동에 익숙합니다.',
+            'Oracle 및 MySql의 쿼리문법에 익숙합니다.',
             'DB 설계 및 정규화 경험이 있습니다.'
           ]
         },
@@ -66,27 +62,25 @@ export default {
       ]
     },
     {
-      title: 'Communication',
+      title: 'Others',
       description: [
         {
-          name: '',
+          name: 'Communication',
           level: '',
           description: [
             '타 부서 혹은 직원간 서로의 업무에 대해 존중하며, 협력하여 좋은 결과를 이끌어 낼 수 있도록 노력합니다.',
-          ]
-        },
-        {
-          name: '',
-          level: '',
-          description: [
-              '의견의 차이를 줄이는 것이야 말로 업무의 효율성을 높이는 방법이라 생각합니다. 서로 다른 의견을 이해하며 하나의 의견으로 모으기 위해 노력합니다.',
-          ]
-        },
-        {
-          name: '',
-          level: '',
-          description: [
+            '의견 충돌 시 서로 다른 의견을 더 나은 의견으로 변화시키려 노력합니다.',
             '피드백 받는 것에 대해 거리낌이 없습니다. 진심어린 피드백은 최고의 성장 비타민이라 생각합니다.'
+          ]
+        },
+        {
+          name: 'Attitude',
+          level: '',
+          description: [
+            '9시 1분은 9시가 아닙니다. 즉 시간약속을 잘 지킵니다.',
+            '기본소양을 토대로 새로움을 탐험하는 것을 즐깁니다.',
+            '남에게 듣는 것 남에게 알려주는 것 모두 좋아합니다.',
+            '운동과 먹는것을 통해 스스로 건강을 챙깁니다.'
           ]
         }
       ]
--- src/models/WorkExperience.js
+++ src/models/WorkExperience.js
@@ -8,34 +8,33 @@ export default {
       position: 'Full-Stack WEB Developer',
       period: '2017. 11 - 2018. 11',
       period_num: '1년 1개월',
-      description: '웹 애플리케이션 개발 (프론트엔드/백엔드)',
+      description: '웹 애플리케이션 개발 (프론트/백 엔드)',
       project: [
         {
           name: '국립민속박물관 민속현장조사 사용자(WEB)',
           period: '2017. 11 - 2018. 1',
-          description: '웹 애플리케이션 개발',
+          description: '웹 애플리케이션 리뉴얼 개발(팀 2명)',
           works: [
-            '팀 단위 개발 : 외부 디자인(외주) 변경에 따른 웹 사이트 개발(본인 외 1명)',
-            'JS 기반(WEB VR, Echart.js, Google Map, tui.editor, flexsheet.js, jsTree 등) 라이브러리 커스터마이징',
-            '웹 접근성(Accessibility), 검색엔진최적화(SEO) 등을 고려하여 HTML구조 재 설계',
-            '테스트 엔지니어 경험을 살려 통합 테스트 진행시 리드(화이트박스 테스팅, 크로스 브라우징)',
-            '검색 기능을 검색엔진(Elastic Search) 검색으로 변경하는 작업 진행'
+            'JS 기반 라이브러리 커스터마이징(WEB VR, Echart.js, Google Map, tui.editor, flexsheet.js 등)',
+            '웹 접근성, 검색엔진최적화(SEO) 등을 고려하여 HTML구조 재 설계',
+            '테스트 엔지니어 경험으로 통합 테스트 진행시 리드(화이트박스 테스팅, 크로스 브라우징)',
+            '검색 기능을 검색엔진(Elastic Search)을 통한 검색으로 변경하는 작업 진행'
           ],
           skill_stack: [
-            'Spring MVC', 'Jsp', 'Jstl', 'jQuery', 'Mybatis', 'Oracle', 'Elasitc Search',
+            'Spring MVC', 'Jsp', 'Jstl', 'jQuery', 'Mybatis', 'Oracle', 'Elastic Search',
             'Maven', 'git'
           ]
         },
         {
           name: '국립민속박물관 민속현장조사 관리자(WEB)',
           period: '2018. 2 - 2018. 4',
-          description: '웹 애플리케이션 개발',
+          description: '웹 애플리케이션 개발(단독)',
           works: [
-            'Spring boot를 사용하여 프레임 워크 설계 및 개발(단독 프로젝트)',
+            'Spring boot를 사용하여 프레임 워크 설계 및 개발',
             '기능의 편리한 사용을 위해 사용자 관점으로 직접 기획, bootstrap 기반 UI 디자인 및 구현',
             'Spring AOP, 트랜잭션, Spring ehcache, Spring Test 등 관련 의존 라이브러리 적용',
             'Spring Security를 커스터마이징하여 적용',
-            'C/S 프로그램과 (다량 업로드) 호환하는 api를 디자인 및 구현'
+            'C/S 프로그램과 호환하는 api를 디자인 및 구현'
           ],
           skill_stack: [
             'Spring boot', 'Jsp', 'Jstl', 'jQuery', 'Bootstrap', 'Mybatis', 'Oracle',
@@ -45,13 +44,14 @@ export default {
         {
           name: 'STONE Q (문제풀이, 성적관리 시스템)',
           period: '2018. 5 - 2018. 6',
-          description: '웹 애플리케이션 개발',
+          description: '웹 애플리케이션 개발(팀 2명)',
           works: [
-            'Vue.js 학습 및 실무 적용',
+            'Vuex, Vue-Router 등 Vue.js 관련 라이브러리 사용 경험',
+            '팀 단위 컴포넌트 개발 경험',
             'REST API URI 디자인 및 구현'
           ],
           skill_stack: [
-            'Spring boot', 'Vue.js', 'mybatis', 'oracle', 'gradle', 'git'
+            'Spring boot', 'Vue.js', 'Sass', 'mybatis', 'oracle', 'gradle', 'Webpack', 'git'
           ]
         }
       ]
@@ -64,13 +64,18 @@ export default {
       position: 'Software Test Engineer',
       period: '2016. 07 - 2016. 08',
       period_num: '2개월',
-      description: '하계인턴 (Naver에서 서비스 중인 웹 및 모바일 애플리케이션 테스트)',
+      description: '글로벌 테스트실/테스트 엔지니어 하계인턴',
       project: [
         {
           name: 'Arcade 서비스 테스팅',
           description: '소프트웨어 테스팅',
-          works: ['팀 단위 개발 : 외부 디자인(외주) 변경에 따른 웹 사이트 개발(본인 외 1명)'],
-          skill_stack: ['software testing']
+          works: [
+            '네이버에서 실제 서비스되는 웹과 모바일의 테스트를 진행',
+            '테스트케이스 작성 및 개발자와 테스터의 협업 경험',
+            '테스트 방법론 및 자동화 테스팅 경험'
+
+          ],
+          skill_stack: ['WhiteBox testing', 'Cross Browsing']
         }
       ]
     }
