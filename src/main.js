5bc4832e4cd0959f4c296413457f23a6363c06e0
luke-hanwook <gksdnr89@gmail.com>
2019-02-02 15:03:54 +0900

    ie promise polyfill 적용

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
+import 'es6-promise/auto'
 import App from './App.vue'
 import store from './store'
+import Meta from 'vue-meta'
 
 import 'normalize.css'
-// import '@/assets/css/main.css'
 import '@/assets/sass/main.scss'
 import '@/assets/sass/media.scss'
+
+Vue.use(Meta)
 
 new Vue({
   el: '#app',
