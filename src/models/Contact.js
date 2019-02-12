c8a7c44789450407a5b82ed04bb2c5b500fc35fa
luke-hanwook <gksdnr89@gmail.com>
2019-02-13 00:19:43 +0900

    contact 수정

--- src/models/Contact.js
+++ src/models/Contact.js
@@ -14,6 +14,11 @@ export default {
       name: '블로그',
       url: 'https://blog.naver.com/gksdnr89',
       type: 'url'
+    },
+    {
+      name: '코드샌드박스',
+      url: 'https://codesandbox.io/u/luke-hanwook',
+      type: 'url'
     }
   ],
   list() {
